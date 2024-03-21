fcff78254747f52fea45bbcb6287b9bc2b87052b
aylinforbes <139925798+aylinforbes@users.noreply.github.com>
2024-03-21 05:18:10 -0700

    first commit

---
+++ CarCollection/src/config/firebase.config.ts
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyCbOojBXst8ZWIrbAUcDgTI0Qg5HR2fnkw",
+    authDomain: "car-collection-df419.firebaseapp.com",
+    projectId: "car-collection-df419",
+    storageBucket: "car-collection-df419.appspot.com",
+    messagingSenderId: "1075261471587",
+    appId: "1:1075261471587:web:3ca53bc5dc510f8d507156"
+  };
+  
